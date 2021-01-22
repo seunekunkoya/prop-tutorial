d73da2b58aad90cfe159183c2a15dde9f83e6e52
Oluwaseun <ekunkoya.seun@gmail.com>
2021-01-22 16:20:40 +0100

    added stuffs

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Product from '../Product/Product'
+
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
